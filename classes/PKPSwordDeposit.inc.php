118fa3b20967219c3a187c7fb4941b8e492b4a14
PedroPinto <81102024+TheMostWnted@users.noreply.github.com>
2025-03-12 15:39:03 +0000

    New Metadata - keywords, issued date and indentifier

--- classes/PKPSwordDeposit.inc.php
+++ classes/PKPSwordDeposit.inc.php
@@ -75,11 +75,52 @@ class PKPSwordDeposit {
 	 * @param $request PKPRequest
 	 */
 	public function setMetadata($request) {
+		$publication = $this->_submission->getCurrentPublication();
 		$this->_package->setCustodian($this->_context->getContactName());
 		$this->_package->setTitle(html_entity_decode($this->_submission->getLocalizedTitle(), ENT_QUOTES, 'UTF-8'));
 		$this->_package->setAbstract(html_entity_decode(strip_tags($this->_submission->getLocalizedAbstract()), ENT_QUOTES, 'UTF-8'));
+
+		if ($publication) {
+			$doi = $publication->getStoredPubId('doi');
+		}
+
+		$dispatcher = $request->getDispatcher();
+		$context = $request->getContext();
+		$submissionId = $this->_submission->getId(); 
+		$articleUrl = $dispatcher->url(
+			$request,
+			ROUTE_PAGE,
+			$context->getPath(),
+			'article',
+			'view',
+			['submissionId' => $submissionId]
+		);
+	
+		// Add DOI or URL to the package
+		 if (!empty($doi)) {
+			$this->_package->setIdentifier($doi);  
+		}
+		else {
+			$this->_package->setIdentifier($articleUrl);
+		}
+
+		// Add Issued Date to the package
+		$date = $this->_submission->getDatePublished();
+		$this->_package->setDateAvailable($date);
+		
+		$keywords = $publication->getData('keywords');
+
+		// Add keywords to the package
+		if (!empty($keywords) && is_array($keywords)) {
+			foreach ($keywords as $locale => $keywordArray) {
+				foreach ($keywordArray as $keyword) {
+					$this->_package->addSubject($keyword);
+				}
+			}
+		}
+
 		$this->_package->setType($this->_section->getLocalizedIdentifyType());
-		$publication = $this->_submission->getCurrentPublication();
+		
 		foreach ($publication->getData('authors') as $author) {
 			$creator = $author->getFullName(true);
 			$affiliation = $author->getLocalizedAffiliation();
@@ -121,13 +162,12 @@ class PKPSwordDeposit {
 	 * @return boolean true iff a file was successfully added to the package
 	 */
 	public function addEditorial() {
-		import('lib.pkp.classes.submission.SubmissionFile');
-		$fileStages = [
+		$fileStages = array(
 			SUBMISSION_FILE_PRODUCTION_READY,
 			SUBMISSION_FILE_COPYEDIT,
 			SUBMISSION_FILE_REVIEW_FILE,
 			SUBMISSION_FILE_SUBMISSION
-		];
+		);
 		$submissionFiles = iterator_to_array(Services::get('submissionFile')->getMany([
 			'submissionIds' => [$this->_submission->getId()],
 			'fileStages' => $fileStages,
